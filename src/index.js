c00baab033dd5d65193a12da2b97301943b8be63
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-06-09 17:33:54 +0200

    playing with json form with inferred schema

--- src/index.js
+++ src/index.js
@@ -2,26 +2,16 @@ import * as scribble from "scribbletune";
 
 const session = new scribble.Session();
 
-const polySynth = new Tone.PolySynth(5, Tone.Synth, {
-  oscillator: {
-    type: "fatsawtooth",
-    count: 5,
-    spread: 20,
-  },
-  envelope: {
-    attack: 0.02,
-    decay: 0.1,
-    sustain: 0.5,
-    release: 0.1,
-  },
-});
+const synthParams = Tone.PolySynth.defaults;
+
+const polySynth = new Tone.PolySynth(5, Tone.Synth, synthParams);
 
 const synthChannel = session.createChannel({
   instrument: polySynth,
   clips: [
     {
-      notes: scribble.getChordsByProgression("C3 major", "V I V V I I"),
-      pattern: "[x---][-[x-][x-][x-]][--x-]x",
+      notes: scribble.getChordsByProgression("C3 major", "V V V V I I I I"),
+      pattern: "[x---][x---][x---][x---]xxxx",
     },
     {
       notes: scribble.getChordsByProgression("C4 major", "I II V I I"),
@@ -32,44 +22,77 @@ const synthChannel = session.createChannel({
 
 session.startRow(0);
 
-const BPM = 120;
-
-Tone.Transport.bpm.value = BPM;
-// Tone.Transport.start();
-
-// Tone.Transport.schedule((time) => session.startRow(1), "3:3:3");
-// Tone.Transport.stop("8:0:0");
-// Tone.Transport.stop("4:0:0");
+Tone.Transport.bpm.value = 120;
 
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Form from "@rjsf/core";
 
-const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: { type: "string", title: "Title", default: "A new task" },
-    done: { type: "boolean", title: "Done?", default: false },
+import toJsonSchema from "to-json-schema";
+import { Container, Col, Button } from "react-bootstrap";
+
+const toJsonOptions = {
+  postProcessFnc: function forceIntegersToBeNumbers(
+    type,
+    schema,
+    value,
+    defaultFunc
+  ) {
+    if (type === "integer" || type === "number") {
+      schema.type = "number";
+      return { ...schema, multipleOf: 0.005 };
+    } else {
+      return defaultFunc(type, schema, value);
+    }
   },
 };
-
-const log = (type) => console.log.bind(console, type);
+const schema = toJsonSchema(synthParams.voice.defaults, toJsonOptions);
 
 const App = () => {
+  var [isPlaying, togglePlay] = React.useState(true);
+  const playPause = function () {
+    togglePlay(!isPlaying);
+    if (isPlaying) {
+      Tone.Transport.start();
+    } else {
+      Tone.Transport.stop();
+    }
+  };
+
+  var [formData, setFormData] = React.useState(synthParams.voice.defaults);
+  const setSynth = function (e) {
+    setFormData(e.formData);
+    polySynth.set(formData);
+  };
+
+  var bpmSchema = { type: "number" };
+  var [bpm, setBpm] = React.useState(Tone.Transport.bpm.value);
+  const setToneBpm = function (e) {
+    setBpm(e.formData);
+    Tone.Transport.bpm.value = bpm;
+  };
+
+  // const uiSchema = {
+  //   envelope: { attack: { "ui.widget": "range" } },
+  // };
+
   return (
-    <div>
-      <div>Voici du react</div>
+    <Container fluid="sm">
+      <div>Schema</div>
+      <Button variant="primary" onClick={playPause}>
+        ▶️ Play
+      </Button>
+      <Form schema={bpmSchema} formData={bpm} onSubmit={setToneBpm} />
       <Form
         schema={schema}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")}
+        formData={formData}
+        // uiSchema={uiSchema}
+        onSubmit={setSynth}
       />
-    </div>
+    </Container>
   );
 };
 
